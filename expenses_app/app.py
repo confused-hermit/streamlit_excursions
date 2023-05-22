2b42c83d80d45fb577f176961a87bf399bc9f226
hermit-confused <hermit.confused@gmail.com>
2023-05-23 04:31:18 +0530

    added plotly chart

--- expenses_app/app.py
+++ expenses_app/app.py
@@ -79,3 +79,21 @@ with st.form("saved_period"):
         col2.metric("Total Expense", f"{total_expense} {currency}")
         col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
         st.text(f"Comment: {comment}")
+
+        # Create a sankey chart
+        label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
+        source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
+        target = [len(incomes)] * len(incomes) + [
+            label.index(expense) for expense in expenses
+        ]
+        value = list(incomes.values()) + list(expenses.values())
+
+        # Data to dict, dict to sankey
+        link = dict(source=source, target=target, value=value)
+        node = dict(label=label, pad=20, thickness=30, color="#E694FF")
+        data = go.Sankey(link=link, node=node)
+
+        # Plot it
+        fig = go.Figure(data)
+        fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
+        st.plotly_chart(fig, use_container_width=True)
